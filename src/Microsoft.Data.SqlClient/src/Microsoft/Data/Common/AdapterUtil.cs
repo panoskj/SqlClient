417ab804fedf9f36d778299eb1f867653c591d08
panoskj <pan.kj95@gmail.com>
2022-03-19 16:21:26 +0200

    Merging TdsParserStateObject.ReadSni method.

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/Common/AdapterUtil.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/Common/AdapterUtil.cs
@@ -416,6 +416,29 @@ namespace Microsoft.Data.Common
             => Argument(StringsHelper.GetString(Strings.ADP_InvalidArgumentLength, argumentName, limit));
 
         internal static ArgumentException MustBeReadOnly(string argumentName) => Argument(StringsHelper.GetString(Strings.ADP_MustBeReadOnly, argumentName));
+        
+        internal static Timer UnsafeCreateTimer(TimerCallback callback, object state, int dueTime, int period)
+        {
+            // Don't capture the current ExecutionContext and its AsyncLocals onto 
+            // a global timer causing them to live forever
+            bool restoreFlow = false;
+            try
+            {
+                if (!ExecutionContext.IsFlowSuppressed())
+                {
+                    ExecutionContext.SuppressFlow();
+                    restoreFlow = true;
+                }
+
+                return new Timer(callback, state, dueTime, period);
+            }
+            finally
+            {
+                // Restore the current ExecutionContext
+                if (restoreFlow)
+                    ExecutionContext.RestoreFlow();
+            }
+        }
 #endregion
 
 #region CommandBuilder, Command, BulkCopy
@@ -1476,29 +1499,6 @@ namespace Microsoft.Data.Common
 #endregion
 #else
 #region netcore project only
-        internal static Timer UnsafeCreateTimer(TimerCallback callback, object state, int dueTime, int period)
-        {
-            // Don't capture the current ExecutionContext and its AsyncLocals onto 
-            // a global timer causing them to live forever
-            bool restoreFlow = false;
-            try
-            {
-                if (!ExecutionContext.IsFlowSuppressed())
-                {
-                    ExecutionContext.SuppressFlow();
-                    restoreFlow = true;
-                }
-
-                return new Timer(callback, state, dueTime, period);
-            }
-            finally
-            {
-                // Restore the current ExecutionContext
-                if (restoreFlow)
-                    ExecutionContext.RestoreFlow();
-            }
-        }
-
         //
         // COM+ exceptions
         //
